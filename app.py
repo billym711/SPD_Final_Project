70b3bdf034b5d12aabc04b737b90976810eafbd4
bbercik <bbercik@iu.edu>
2024-11-11 16:07:44 -0500

    index

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
     conn = get_db_connection()
     if request.method == "POST":
